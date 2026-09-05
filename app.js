3357893530f28c8765d380b30b7ca3312f81bc99
georgebrian7 <brianmecha021@gmail.com>
2026-09-05 13:04:41 +0300

    Stage 1: root and health endpoints

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const port=3000;
 
-
-const http =require('http');
-const server = http.createServer((req, res)=>{
-    if (req.url ==='/'){
-        res.write('Hello World');
-        res.end();
-    }
+app.get('/', (req,res)=>{
+    res.json({ "name":"Task API", "version":"1.0","endpoints":["/tasks"]})
+});
+app.get('/health',(req,res)=>{
+    res.json({"status":"ok"})
 });
 
-server.on('connection', (socket)=>{
-    console.log('new connection');
+app.listen(port,()=>{
+    console.log('listening on port 3000')
 });
-server.listen(3000);
 
-console.log('Listening on port 3000')
+// const http =require('http');
+// const server = http.createServer((req, res)=>{
+//     if (req.url ==='/'){
+//         res.write('Hello World');
+//         res.end();
+//     }
+// });
+
+// server.on('connection', (socket)=>{
+//     console.log('new connection');
+// });
+// server.listen(3000);
+
+// console.log('Listening on port 3000')
